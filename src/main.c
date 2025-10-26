628fa410411b5a47cd8c95c4bca61ef63e6ab6cb
Jim Chen <theyee0@gmail.com>
2025-10-26 15:46:56 -0400

    Added fluid simulation demo to main

--- src/main.c
+++ src/main.c
@@ -1,21 +1,22 @@
 #include "raylib.h"
 #include "movement.h"
+#include "fluids.h"
 #include <stdlib.h>
+#include <string.h>
+
+void get_fluid_model_input(struct fluid_model *fluid, float virtual_ratio);
 
 int main(void)
 {
-        int i;
-
         const int screen_width = 1024;
-        const int screen_height = 768;
-        const int virtual_screen_width = 320;
-        const int virtual_screen_height = 240;
+        const int screen_height = 1024;
+        const int virtual_screen_width = 256;
+        const int virtual_screen_height = 256;
 
-        double time;
-
-        const float virtual_ratio = (float)screen_width / (float)screen_height;
+        const float virtual_ratio = (float)screen_width / (float)virtual_screen_width;
 
         InitWindow(screen_width, screen_height, "Auburn");
+        SetTargetFPS(24);
 
         RenderTexture2D target = LoadRenderTexture(virtual_screen_width, virtual_screen_height);
 
@@ -41,30 +42,46 @@ int main(void)
         world_camera.zoom = 1.0f;
         screen_camera.zoom = 1.0f;
 
-        struct leaf *leaves;
-        int num_leaves = 1;
+        struct fluid_model *fluid = NULL;
+        struct fluid_model *delta = NULL;
 
-        leaves = malloc(1024 * sizeof(*leaves));
+        init_model(&fluid, virtual_screen_width, virtual_screen_height, 0.0f, 0.0f);
+        init_model(&delta, virtual_screen_width, virtual_screen_height, 0.0f, 0.0f);
 
-        init_leaf(&leaves[0], 80, 0);
+        zero_buffer(fluid, fluid->density);
+        zero_buffer(fluid, fluid->velocity_x);
+        zero_buffer(fluid, fluid->velocity_y);
+
+        for (int i = 1; i <= fluid->height; i++) {
+                for (int j = 1; j <= fluid->width / 2; j++) {
+                        fluid->density[i][j] = 1;
+                }
+        }
 
         while (!WindowShouldClose()) {
-                for (i = 0; i < num_leaves; i++) {
-                        leaves[i].acceleration.x = 0;
-                        leaves[i].acceleration.y = 0;
-                        apply_force(&leaves[i], calc_gravity(leaves[i]));
-                        apply_force(&leaves[i], calc_drag(leaves[i]));
-                }
+                get_fluid_model_input(delta, virtual_ratio);
+
+                step_velocity(&fluid, &delta, GetFrameTime());
+                step_density(&fluid, &delta, GetFrameTime());
 
                 BeginTextureMode(target);
                 ClearBackground(RAYWHITE);
                 BeginMode2D(world_camera);
-                for (i = 0; i < num_leaves; i++) {
-                        calc_position(&leaves[i], GetFrameTime());
-                        DrawCircle((int)(leaves[i].position.x * PIXELS_PER_METER),
-                                   (int)(leaves[i].position.y * PIXELS_PER_METER),
-                               leaves[i].size, leaves[i].color);
+
+                for (int i = 1; i <= virtual_screen_width; i++) {
+                        for (int j = 1; j <= virtual_screen_width; j++) {
+                                if (fluid->density[i][j] > 1) {
+                                        DrawPixel(i, j, BLACK);
+                                } else if (fluid->density[i][j] > 0.75) {
+                                        DrawPixel(i, j, DARKGRAY);
+                                } else if (fluid->density[i][j] > 0.5) {
+                                        DrawPixel(i, j, GRAY);
+                                } else if (fluid->density[i][j] > 0.25) {
+                                        DrawPixel(i, j, LIGHTGRAY);
+                                }
+                        }
                 }
+
                 EndMode2D();
                 EndTextureMode();
 
@@ -80,4 +97,22 @@ int main(void)
         CloseWindow();
 
         return 0;
+}
+
+void get_fluid_model_input(struct fluid_model *fluid, float virtual_ratio) {
+        Vector2 d;
+        Vector2 p = GetMousePosition();
+
+        p.x /= virtual_ratio;
+        p.y /= virtual_ratio;
+
+        zero_buffer(fluid, fluid->density);
+        zero_buffer(fluid, fluid->velocity_x);
+        zero_buffer(fluid, fluid->velocity_y);
+
+        d = GetMouseDelta();
+        fluid->velocity_x[(int)p.x][(int)p.y] = d.x / GetFrameTime();
+        fluid->velocity_y[(int)p.x][(int)p.y] = d.y / GetFrameTime();
+
+        fluid->density[(int)p.x][(int)p.y] = 0;
 }
